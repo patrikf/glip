b64ef70d485c5a214b69e85bc77fc00f050ed387
Patrik Fimml <patrik@fimml.at>
2009-05-31 14:16:15 +0200

    Add licensing information to every source file

--- lib/binary.class.php
+++ lib/binary.class.php
@@ -1,10 +1,25 @@
 <?php
-
 /*
  * binary.class.php:
  * Utility functions for dealing with binary files/strings.
- *
  * All functions assume network byte order (big-endian).
+ *
+ * Copyright (C) 2008, 2009 Patrik Fimml
+ *
+ * This file is part of glip.
+ *
+ * glip is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+
+ * glip is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with glip.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 final class Binary
--- lib/git_commit_stamp.class.php
+++ lib/git_commit_stamp.class.php
@@ -1,4 +1,22 @@
 <?php
+/*
+ * Copyright (C) 2008 Patrik Fimml
+ *
+ * This file is part of glip.
+ *
+ * glip is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+
+ * glip is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with glip.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 class GitCommitStamp
 {
